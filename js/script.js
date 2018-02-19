8d95a9b52007472a3478c5dd2a16705425bb0753
David Kröll <david.kroell@outlook.com>
2018-02-19 20:04:39 +0100

    minimize dependencies

--- js/script.js
+++ js/script.js
@@ -6,6 +6,6 @@ function __init() {
 
     /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
     particlesJS.load('particles-js', 'js/particles.json', function() {
-        console.log('callback - particles.js config loaded');
+        console.log('particles loaded');
     });
 }
