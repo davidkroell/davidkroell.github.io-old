d860903d4bc58e8550047b5595c13c3716b0e9bc
David Kröll <david.kroell@outlook.com>
2018-04-29 16:31:25 +0200

    add timeline; litte change in color

--- js/more.js
+++ js/more.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     // MODAL
     $('.sidenav').sidenav();
-    $('.fixed-action-btn').floatingActionButton();
     $('.tooltipped').tooltip();
 });
 
 var header = $('header');
+var navBg = $('#nav-bg');
 var range = 200;
 
 $(window).on('scroll', function () {
@@ -16,11 +16,15 @@ $(window).on('scroll', function () {
         calc = 1 - (scrollTop - offset + range) / range;
 
     header.css({ 'opacity': calc });
+    navBg.css({ 'opacity': 1 - calc });
 
     if (calc > '1') {
         header.css({ 'opacity': 1 });
+        navBg.css({ 'opacity': 0 });
     } else if (calc < '0') {
         header.css({ 'opacity': 0 });
+        navBg.css({ 'opacity': 1 });
+
     }
 
 });
